85d856d9a86c7346e7c68bfee0fe6311a9e0f928
Isaac Cruz <cruzisaac51@gmail.com>
2022-12-30 17:16:15 -0600

    signup

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("projects/", views.projects, name="Projects"),
     path("tasks/", views.tasks, name="Tasks"),
     path("create_task/", views.create_task, name= "create_new_Task"),
-    path("create_project/", views.create_project, name= "create_new_Project")
+    path("create_project/", views.create_project, name= "create_new_Project"),
+    path("signup/", views.signup, name="Signup"),
 ]   
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
 from django.shortcuts import get_object_or_404
 from .forms import CreateNewTask
 from .forms import CreateNewProject
+
+
 # Create your views here.
 
 def index(request):
@@ -48,5 +52,30 @@ def create_project(request):
     else:
         Project.objects.create(name= request.POST["name"])
         redirect("Projects")
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, "user/signup.html", {
+        'form' : UserCreationForm
+        })
+    
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username= request.POST['username'], password= request.POST['password1'])
+                user.save()
+                return HttpResponse("user created successfully")
+            except:
+                return render(request, "user/signup.html", {
+                    'form' : UserCreationForm,
+                    'error': 'username already exists'
+                })
+        return render(request, "user/signup.html", {
+                    'form' : UserCreationForm,
+                    'error': 'Password is incorrect'
+                })
+
+    
 
 
